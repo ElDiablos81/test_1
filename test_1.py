f3277d7e3404792082b62cc7dc538345b0d3e05c
maximrychkov <maxim.rychkov@gmail.com>
2021-05-01 10:48:38 +0300

    new test1

--- test_1.py
+++ test_1.py
@@ -1,7 +1,4 @@
-# contents of test_player_mechanics.py
-def test_player_hit():
-    player = create_player()
-    assert player.health == 100
-    undead = create_undead()
-    undead.hit(player)
-    assert player.health == 80
+def test_default_health():
+    health = 95
+    assert health == 95
+    assert 95 == 95
