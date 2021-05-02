3d5b213a680e230c5f64865b4391078eba5b42d3
maximrychkov <maxim.rychkov@gmail.com>
2021-05-02 09:25:22 +0300

    new test3

--- test_1.py
+++ test_1.py
@@ -1,8 +1,26 @@
 def mul(a, b):
+    """
+    :param a:2
+    :param b:3
+    :return:6
+    """
     return a * b
 
+def add(a, b):
+    """
+>>> add(2, 3)
+5
+
+>>> add('a', 'b')
+'ab'
+    """
+    return a + b
+
 def test_mul_true():
-    assert mul(2,2) == 4
+    assert mul(2,3) == 6
 
 def test_mul_false():
     assert mul(2,2) == 5
+
+def test_add_true():
+    assert add('a', 'b') == 'ab'
