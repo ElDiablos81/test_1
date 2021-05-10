622176e17abe4530fcb184e6e91cd810f3b94eb9
maximrychkov <maxim.rychkov@gmail.com>
2021-05-10 10:14:33 +0300

    new test000

--- test_2.py
+++ test_2.py
@@ -20,12 +20,12 @@ class MyTestCase(unittest.TestCase):
 
     def test_case03(self):
         print("\nRunning Test Method : " + inspect.stack()[0][3])
-        self.assertEqual(add(2, 2), 5)
+        self.assertEqual(add(2, 2), 4)
 
     def test_case04(self):
         print("\nRunning Test Method : " + inspect.stack()[0][3])
         my_var = 3.14
-        self.assertTrue(isinstance(my_var, int))
+        self.assertFalse(isinstance(my_var, int))
 
 
 class TestClass04(unittest.TestCase):
